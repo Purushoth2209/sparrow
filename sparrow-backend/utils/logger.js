dc0a9bc6cb814628fbba55bed6eb96af90de7cb9
Purushoth2209 <purushothamanramalingam22@gmail.com>
2025-10-24 11:17:27 +0530

    updated time stamp

--- sparrow-backend/utils/logger.js
+++ sparrow-backend/utils/logger.js
@@ -1,10 +1,19 @@
 // Custom logging utility with timestamps
+// Store original console methods to avoid circular reference
+const originalConsole = {
+  log: console.log.bind(console),
+  error: console.error.bind(console),
+  warn: console.warn.bind(console),
+  info: console.info.bind(console),
+  debug: console.debug.bind(console)
+};
+
 const logWithTimestamp = {
-  log: (...args) => console.log(`[${new Date().toISOString()}]`, ...args),
-  error: (...args) => console.error(`[${new Date().toISOString()}]`, ...args),
-  warn: (...args) => console.warn(`[${new Date().toISOString()}]`, ...args),
-  info: (...args) => console.info(`[${new Date().toISOString()}]`, ...args),
-  debug: (...args) => console.debug(`[${new Date().toISOString()}]`, ...args)
+  log: (...args) => originalConsole.log(`[${new Date().toISOString()}]`, ...args),
+  error: (...args) => originalConsole.error(`[${new Date().toISOString()}]`, ...args),
+  warn: (...args) => originalConsole.warn(`[${new Date().toISOString()}]`, ...args),
+  info: (...args) => originalConsole.info(`[${new Date().toISOString()}]`, ...args),
+  debug: (...args) => originalConsole.debug(`[${new Date().toISOString()}]`, ...args)
 };
 
 module.exports = logWithTimestamp;
